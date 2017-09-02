8d174397b49bf4ce53636906c08f5649e258b30f
Paul Gowder <paul.gowder@gmail.com>
2017-09-02 17:48:07 -0500

    got my regex limited to something reasonable

--- browser/footnotes.js
+++ browser/footnotes.js
@@ -23,14 +23,32 @@ function addtoDom(paragraph){
     document.getElementsByTagName("body")[0].appendChild(newParagraph);
 }
 
+function iterateRegexes(fntext, sctext){
+    var rs1Demanding = "see,?\\s?(?:generally|e\\.g\\.)?.*?";
+    var rs1LessDemanding = "(?:see|\\n\\s?|\\.\\s*),?\\s?(?:generally|e\\.g\\.)?.*?";
+    var rs1Forgiving =  "(?:see|\\n\\s?|\\.\\s*|^),?\\s?(?:generally|e\\.g\\.)?.*?";
+    var rs3 = ".*?\\(.*?(?:\\d\\d\\d\\d|forthcoming)\\)";
+    var demanding = rs1Demanding + _.escapeRegExp(sctext) + rs3;
+    var lessDemanding = rs1LessDemanding + _.escapeRegExp(sctext) + rs3;
+    var forgiving = rs1Forgiving + _.escapeRegExp(sctext) + rs3;
+    for (let rstring of [demanding, lessDemanding, forgiving]){
+        let regex = new RegExp(rstring, "i");
+        let match = fntext.match(regex);
+        if (match){
+            return match;
+        }
+    }
+}
+
 function findCitation(fntext, sctext){
-    var rs1 = "(?:see|\\n\\s?|\\.\\s*|^),?\\s?(?:generally|e\\.g\\.)?.*?"
-    var rs3 = ".*?\\(.*?(?:\\d\\d\\d\\d|forthcoming)\\)"
-    var rstring = rs1 + _.escapeRegExp(sctext) + rs3;
-    var regex = new RegExp(rstring, "i");
-    addtoDom(rstring);
+    //var rs1 = "(?:see|\\n\\s?|\\.\\s*|^),?\\s?(?:generally|e\\.g\\.)?.*?"
+    //var rs3 = ".*?\\(.*?(?:\\d\\d\\d\\d|forthcoming)\\)"
+    //var rstring = rs1 + _.escapeRegExp(sctext) + rs3;
+    //var regex = new RegExp(rstring, "i");
+    //addtoDom(rstring);
+    var match = iterateRegexes(fntext, sctext);
     addtoDom(fntext);
-    addtoDom(JSON.stringify(fntext.match(regex)));
+    addtoDom(JSON.stringify(match));
 }
 
 function extractFromFootnote(footnote){
