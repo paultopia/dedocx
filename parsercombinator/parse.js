bfedcc7d216d1b1e829eaea04eb096d025ab7752
Paul Gowder <paul.gowder@gmail.com>
2017-09-06 11:59:56 -0500

    setup a little

---
+++ parsercombinator/parse.js
@@ -0,0 +1,8 @@
+const Parjs = require('parjs').Parjs;
+const fs = require("fs");
+const samples = require("./samples.json");
+console.log(samples[0]);
+console.log("\n");
+console.log(samples[1]);
+console.log("\n");
+console.log(samples[2]);
